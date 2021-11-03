31ffcf1cd448151ae7f31febe0f3a68e39a285af
Petr2020 <peterguanjun1992@gmail.com>
2021-11-03 18:11:37 +0800

    Initial Commit

---
+++ SwiftMathRender/SwiftMathRender.h
@@ -0,0 +1,18 @@
+//
+//  SwiftMathRender.h
+//  SwiftMathRender
+//
+//  Created by Petr Guan on 2021/11/3.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftMathRender.
+FOUNDATION_EXPORT double SwiftMathRenderVersionNumber;
+
+//! Project version string for SwiftMathRender.
+FOUNDATION_EXPORT const unsigned char SwiftMathRenderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMathRender/PublicHeader.h>
+
+
